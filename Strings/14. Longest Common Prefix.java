f05c304d035af081aa262a829683c7a9fcd3988d
Sayem <sayemsoftd@gmail.com>
2025-07-27 22:05:56 +0600

    strings

--- Strings/14. Longest Common Prefix.java
+++ Strings/14. Longest Common Prefix.java
@@ -1,31 +1,21 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // first approch
-        // /*
-        StringBuilder result = new StringBuilder () ;
+        StringBuilder result = new StringBuilder("") ;
 
-        Arrays.sort(strs) ;
+        Arrays.sort (strs) ;
 
-        String f = strs[0] ;
-        String l = strs[strs.length-1] ;
+        int i = 0 ;
 
-        for (int i = 0 ; i < Math.min(f.length(), strs[strs.length-1].length()) ; i++)
+        while (i < strs[0].length() && i < strs[strs.length - 1].length())
         {
-            if (f.charAt(i) != l.charAt(i))
-                return result.toString() ;
-                // return String.valueOf(result) ;
-
+            if (strs[0].charAt(i) == strs[strs.length - 1].charAt(i))
+                result.append (strs[0].charAt(i)) ;
+            else
+                break ;
             
-            result.append(f.charAt(i)) ;
+            i++ ;
         }
 
-        return String.valueOf(result) ;
-        // return result.toString ;
-
-        // */
-
-
-        // Second Try -
-        // Optimized way no need to sorting the array
+        return result.toString() ;
     }
 }
