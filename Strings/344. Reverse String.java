202a8e7fec72a1ae5b0319817c719b6b4cfa26c9
Sayem <sayemsoftd@gmail.com>
2025-07-27 22:44:08 +0600

    strings

--- Strings/344. Reverse String.java
+++ Strings/344. Reverse String.java
@@ -4,15 +4,9 @@ class Solution {
         
         for (int i = 0 ; i < size/2 ; i++)
         {
-            // swap (s[i], s[size-1-i]) ;
             char temp = s[i] ;
             s[i] = s[size-1-i] ;
             s[size-1-i] = temp ;
-        }
-
-        // for (int i = size-1 ; i >= 0 ; i--)
-        //     // return s[i] ;
-        //     System.out.print () ;
-       
+        }     
     }
 }
